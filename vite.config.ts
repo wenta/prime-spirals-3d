27c20cd2ac9f7ea2b908193c2106a7d63657fa32
wenta <jakubwenta@gmail.com>
2025-09-01 07:35:03 +0200

    update url

--- vite.config.ts
+++ vite.config.ts
@@ -3,5 +3,5 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/prime-spirals-3d/',
+  base: 'https://wenta.github.io/prime-spirals-3d/',
 })
